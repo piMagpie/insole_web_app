0409bae2d82531676eddf8886e51ae5c802177cf
agazor <AgazorSuRoot@gmail.com>
2019-05-30 16:58:35 +0300

    concept template

--- users/signals.py
+++ users/signals.py
@@ -4,7 +4,12 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=User) # when a user is saved call this function
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User) # when a user is saved call this function
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+ 
